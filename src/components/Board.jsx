c03a24fc1c2571f710d657fa5d2d1aa773e257c5
markglasman <glasman.mark@gmail.com>
2024-08-16 15:18:04 -0400

    used css to create board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,15 +2,15 @@
 import Strike from "./Strike";
 import Tile from "./Tile";
 function Board() {
-    return <div> 
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
+    return <div className="board"> 
+        <Tile className='right-border bottom-border'/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='bottom-border'/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='bottom-border'/>
+        <Tile className='right-border'/>
+        <Tile className='right-border'/>
         <Tile />
         <Strike />
     </div>;
