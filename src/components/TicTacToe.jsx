ed58cfa2eec6dc599345fc4f2c539333d4018e41
markglasman <glasman.mark@gmail.com>
2024-08-18 19:57:21 -0400

    added values to tiles

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,10 +1,12 @@
-import Board from "./Board"
+import { useState } from "react";
+import Board from "./Board";
 
 function TicTacToe() {
+    const [tiles, setTiles] = useState(Array(9).fill(null));
     return (
         <div>
             <h1>Tic Tac Toe</h1>
-            <Board />
+            <Board tiles={tiles} />
         </div>
     )
 }
