a023990b598b4cba01b39ba9ec518dd81a845201
markglasman <glasman.mark@gmail.com>
2024-08-18 20:14:47 -0400

    added onClick functionality to tiles

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -6,11 +6,16 @@ const PLAYER_O = "O";
 
 function TicTacToe() {
     const [tiles, setTiles] = useState(Array(9).fill(null));
-    const [playerTurn, setPlayerTurn] = useState()
+    const [playerTurn, setPlayerTurn] = useState(PLAYER_X)
+
+    const handleTileClick = (index) => {
+        console.log(index);
+    }
+
     return (
         <div>
             <h1>Tic Tac Toe</h1>
-            <Board tiles={tiles} />
+            <Board tiles={tiles} onTileClick={handleTileClick} />
         </div>
     )
 }
