ef8c0dba8dc7fb3fa51db018483f81ff99b2b8fc
markglasman <glasman.mark@gmail.com>
2024-08-18 20:01:30 -0400

    defined players and player turns

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import Board from "./Board";
 
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+
 function TicTacToe() {
     const [tiles, setTiles] = useState(Array(9).fill(null));
+    const [playerTurn, setPlayerTurn] = useState()
     return (
         <div>
             <h1>Tic Tac Toe</h1>
