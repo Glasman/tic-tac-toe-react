603eb1dd32181e577482a11321a4a484746c4eb2
markglasman <glasman.mark@gmail.com>
2024-08-19 13:35:30 -0400

    formatted, created strikeClass useState

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -5,29 +5,35 @@ const PLAYER_X = "X";
 const PLAYER_O = "O";
 
 function TicTacToe() {
-    const [tiles, setTiles] = useState(Array(9).fill(null));
-    const [playerTurn, setPlayerTurn] = useState(PLAYER_X)
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+  const [strikeClass, setStrikeClass] = useState();
 
-    const handleTileClick = (index) => {
-        if(tiles[index] !== null){
-            return;
-        }
-
-       const newTiles = [...tiles];
-       newTiles[index] = playerTurn;
-       setTiles(newTiles);
-       if(playerTurn === PLAYER_X){
-        setPlayerTurn(PLAYER_O);
-       } else {
-        setPlayerTurn(PLAYER_X);
-       }
+  const handleTileClick = (index) => {
+    if (tiles[index] !== null) {
+      return;
     }
 
-    return (
-        <div>
-            <h1>Tic Tac Toe</h1>
-            <Board playerTurn={playerTurn} tiles={tiles} onTileClick={handleTileClick} />
-        </div>
-    )
+    const newTiles = [...tiles];
+    newTiles[index] = playerTurn;
+    setTiles(newTiles);
+    if (playerTurn === PLAYER_X) {
+      setPlayerTurn(PLAYER_O);
+    } else {
+      setPlayerTurn(PLAYER_X);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board
+        strikeClass={strikeClass}
+        playerTurn={playerTurn}
+        tiles={tiles}
+        onTileClick={handleTileClick}
+      />
+    </div>
+  );
 }
-export default TicTacToe
+export default TicTacToe;
